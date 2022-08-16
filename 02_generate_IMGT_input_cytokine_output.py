fc4731c6388532b547cf810c249999dc551d7c11
Emma Bishop <bishemma1@gmail.com>
2022-08-16 14:06:06 -0700

    Fix indentation
    Fixes errors from improper indentation (such as after if statements) and
    inconsistent use of tabs and spaces (I used spaces).

--- 02_generate_IMGT_input_cytokine_output.py
+++ 02_generate_IMGT_input_cytokine_output.py
@@ -21,39 +21,39 @@ import subprocess
 
 # sub-thread for processing one file
 def parse_file(filename, blast_cytokines):
-        # if option for blasting each read to identify cytokine reads is set
-        # generate temporary file without cytokine reads and proceed with it
-        if blast_cytokines:
-            tmp_file, cytokine_list = CdrExtraction.FileWithoutCytokines(filename)
-            possible_TCR_list, empty_cytokine_list = CdrExtraction.ParseWell(tmp_file)
-            os.unlink(tmp_file) 
-        else:
-	    possible_TCR_list, cytokine_list = CdrExtraction.ParseWell(filename)
+    # if option for blasting each read to identify cytokine reads is set
+    # generate temporary file without cytokine reads and proceed with it
+    if blast_cytokines:
+        tmp_file, cytokine_list = CdrExtraction.FileWithoutCytokines(filename)
+        possible_TCR_list, empty_cytokine_list = CdrExtraction.ParseWell(tmp_file)
+        os.unlink(tmp_file) 
+    else:
+        possible_TCR_list, cytokine_list = CdrExtraction.ParseWell(filename)
 
         out_imgt.write(CdrExtraction.HighV_QuestInput(filename.split('.')[0], possible_TCR_list))
-	out_cytokine.write(CdrExtraction.CytokineOutput(filename.split('.')[0], cytokine_list))
+    out_cytokine.write(CdrExtraction.CytokineOutput(filename.split('.')[0], cytokine_list))
 
 # main thread
 if __name__ == '__main__':
-	# parsing arguments
-	parser = argparse.ArgumentParser(description='Process files containing sequencing reads.')
-	parser.add_argument('--imgt_input', required=True, help='File that will contain input for IMGT High/V-Quest')
-	parser.add_argument('--cytokine_output', required=True, help='File that will contain output of cytokine reads')
-        parser.add_argument('-b','--blast_cytokines', help='Blast each read to identify cytokine reads', action='store_true')
-	args = parser.parse_args()
+    # parsing arguments
+    parser = argparse.ArgumentParser(description='Process files containing sequencing reads.')
+    parser.add_argument('--imgt_input', required=True, help='File that will contain input for IMGT High/V-Quest')
+    parser.add_argument('--cytokine_output', required=True, help='File that will contain output of cytokine reads')
+    parser.add_argument('-b','--blast_cytokines', help='Blast each read to identify cytokine reads', action='store_true')
+    args = parser.parse_args()
 
-	# files to be written
-	out_imgt = open(args.imgt_input, 'w',0)
-	out_cytokine = open(args.cytokine_output, 'w',0)
-	out_cytokine.write ('Well\t' + '\t'.join(CdrExtractionOptions.CYTOKINE_LIST.keys()) + '\n')
+    # files to be written
+    out_imgt = open(args.imgt_input, 'w',0)
+    out_cytokine = open(args.cytokine_output, 'w',0)
+    out_cytokine.write ('Well\t' + '\t'.join(CdrExtractionOptions.CYTOKINE_LIST.keys()) + '\n')
 
-	# starting sub-threads
-	pool = multiprocessing.Pool()
-	for filename in sorted(glob.glob('*.fasta')):
+    # starting sub-threads
+    pool = multiprocessing.Pool()
+    for filename in sorted(glob.glob('*.fasta')):
             pool.apply_async(parse_file, args=(filename, args.blast_cytokines))
 
-	# clean up
-	pool.close()
-	pool.join()
-	out_imgt.close()
-	out_cytokine.close()
+    # clean up
+    pool.close()
+    pool.join()
+    out_imgt.close()
+    out_cytokine.close()
